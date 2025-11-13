7d0bce6a6eb769fc55d49b983d816aaa7c4922e2
Luke Bekemeyer <luke.bekemeyer@wustl.edu>
2025-11-12 18:32:59 -0600

    Lifting state up again - added game function

--- src/App.jsx
+++ src/App.jsx
@@ -123,3 +123,16 @@ export default function Board() {
     </>
   );
 }
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
